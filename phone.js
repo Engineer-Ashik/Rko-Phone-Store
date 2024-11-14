5e474b0a8248e6081c184b047bc19fa47cf6f80e
Ashik <engineerashikur@gmail.com>
2024-11-14 10:52:55 +0600

    Added the display function and showing the cards dynamically

--- phone.js
+++ phone.js
@@ -2,16 +2,37 @@
 console.log("hello ashik");
 
 //Initialize the data fetch
-function handlefetch(){
+function handlefetch() {
     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-    .then(res => res.json())
-    .then(data => display(data));
+        .then(res => res.json())
+        .then(data => display(data));
 }
 //display Function
-function display(input){
-    const phones = input.data;
+function display(input) {
+    const phones = input.data; //taking obj data's from the array
+    const divContainer = document.getElementById('displayContainer');
     phones.forEach(phone => {
-        console.log("PHONES ARE ",phone);
+        console.log("PHONES ARE ", phone);
+        //console.log("The Length is : ", phones.length);
+        const phoneCard = document.createElement('div');
+        phoneCard.innerHTML = `
+        <div class="card bg-transparent w-96 shadow-xl">
+  <figure class="px-10 pt-10">
+    <img
+      src="${phone.image}"
+      alt="Phone-image"
+      class="rounded-xl" />
+  </figure>
+  <div class="card-body items-center text-center">
+    <h2 class="card-title text-slate-900 font-extrabold">${phone.brand}</h2>
+    <p class="text-slate-700 font-semibold">${phone.phone_name}</p>
+    <div class="card-actions">
+      <button class="btn btn-primary bg-purple-400 hover:bg-green-600 font-bold">Show Details</button>
+    </div>
+  </div>
+        </div>
+        `
+        divContainer.appendChild(phoneCard);
     });
 
 }
