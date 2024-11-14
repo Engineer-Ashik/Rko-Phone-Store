e08615f1fa9aef4870d82f68bf99d35a1ac80ebd
Ashik <engineerashikur@gmail.com>
2024-11-14 18:54:34 +0600

    added the display clear codeline inside display function

--- phone.js
+++ phone.js
@@ -7,7 +7,6 @@ document.getElementById('button-search').addEventListener('click', function Sear
     const SearchInput = document.getElementById('input-search');
     const SearchValue = SearchInput.value ;
     SearchInput.value= "";
-    console.log(SearchValue);
     handlefetch(SearchValue);
 });
 
@@ -22,38 +21,33 @@ function handlefetch(SearchText) {
 function display(input) {
     const phones = input.data; //taking obj data's from the array
     const divContainer = document.getElementById('displayContainer');
+    divContainer.textContent=''; //clear the previous displayed values here
+  
     phones.forEach(phone => {
         console.log("PHONES ARE ", phone);
         //console.log("The Length is : ", phones.length);
         const phoneCard = document.createElement('div');
         phoneCard.innerHTML = `
         <div class="card bg-transparent w-96 shadow-xl">
-  <figure class="px-10 pt-10">
-    <img
-      src="${phone.image}"
-      alt="Phone-image"
-      class="rounded-xl" />
-  </figure>
-  <div class="card-body items-center text-center">
-    <h2 class="card-title text-slate-900 font-extrabold">${phone.brand}</h2>
-    <p class="text-slate-700 font-semibold">${phone.phone_name}</p>
-    <div class="card-actions">
-      <button class="btn btn-primary bg-purple-400 hover:bg-green-600 font-bold">Show Details</button>
-    </div>
-  </div>
+          <figure class="px-10 pt-10">
+            <img src="${phone.image}" alt="Phone-image" class="rounded-xl"/>
+          </figure>
+          
+          <div class="card-body items-center text-center">
+            <h2 class="card-title text-slate-900 font-extrabold">${phone.brand}</h2>
+            <p class="text-slate-700 font-semibold">${phone.phone_name}</p>
+            <div class="card-actions">
+              <button class="btn btn-primary bg-purple-400 hover:bg-green-600 font-bold">Show Details</button>
+            </div>
+          </div>
         </div>
         `
         divContainer.appendChild(phoneCard);
     });
+    
 }
 
-
-
-
-
-
-
-
+//Loading symbolic Function
 
 
 //Default calling the Functions
