bfc570f32c98e0ac9371bae1fa03e764ef107688
Ashik <engineerashikur@gmail.com>
2024-11-14 09:48:40 +0600

    added the display function and card code initially in HTML

--- phone.js
+++ phone.js
@@ -1,1 +1,28 @@
+//Checking the Java Script Connection
 console.log("hello ashik");
+
+//Initialize the data fetch
+function handlefetch(){
+    fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    .then(res => res.json())
+    .then(data => display(data));
+}
+//display Function
+function display(input){
+    const phones = input.data;
+    phones.forEach(phone => {
+        console.log("PHONES ARE ",phone);
+    });
+
+}
+
+
+
+
+
+
+
+
+
+//Default calling the Functions
+handlefetch();
